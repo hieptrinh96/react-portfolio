0d455e743f5e1f5551a5029bf76d574a373dd295
HiepTrinh <xmikecolx@gmail.com>
2022-11-18 15:58:14 -0800

    change size of icon

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -8,9 +8,11 @@ const Contact = () => {
     <div className="contact-container">
       <img src={secondPic} alt="" />
       <p>Please feel free to contact me!</p>
-      <a href="https://www.linkedin.com/in/hieptrinh/" target="_blank" rel="noopener noreferrer"><img src={linkedInIcon} alt="" /></a>
-      <a href="https://github.com/hieptrinh96" target="_blank" rel="noopener noreferrer"><img src={githubIcon} alt="" /></a>
-      <a href="mailto: xmikecolx@gmail.com" target="_blank" rel="noopener noreferrer"><img src={gmailIcon} alt="" /></a>
+      <div className="icon-container">
+        <a href="https://www.linkedin.com/in/hieptrinh/" target="_blank" rel="noopener noreferrer"><img src={linkedInIcon} alt="" /></a>
+        <a href="https://github.com/hieptrinh96" target="_blank" rel="noopener noreferrer"><img src={githubIcon} alt="" /></a>
+        <a href="mailto: xmikecolx@gmail.com" target="_blank" rel="noopener noreferrer"><img src={gmailIcon} alt="" /></a>
+      </div>
     </div>
   );
 }
